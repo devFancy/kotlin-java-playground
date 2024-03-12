22ae07945b8c43a34d9f8721484684edecc5943a
devfancy <fancy.junyongmoon@gmail.com>
2024-03-12 18:52:14 +0900

    Initial commit

---
+++ java-to-kotlin/src/main/kotlin/lec02/Person.java
@@ -0,0 +1,20 @@
+package lec02;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Person {
+
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @NotNull
+    public String getName() {
+        return name;
+    }
+
+}
+
+
