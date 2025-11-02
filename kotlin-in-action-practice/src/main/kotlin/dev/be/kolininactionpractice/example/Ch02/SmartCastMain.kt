7ac689a694e33c8385970620fa9af41ef682bf3d
devfancy <fancy.junyongmoon@gmail.com>
2025-11-03 00:17:04 +0900

    refactor: " Kotlin in Action 2/E - 2장 연습 "

--- kotlin-in-action-practice/src/main/kotlin/dev/be/kolininactionpractice/example/Ch02/SmartCastMain.kt
+++ kotlin-in-action-practice/src/main/kotlin/dev/be/kolininactionpractice/example/Ch02/SmartCastMain.kt
@@ -1,10 +1,10 @@
 package dev.be.kolininactionpractice.example.Ch02
 
 interface Expr
+// Num, Sum 에 있는 프로퍼티가 val 로 지정되어 있으므로 스마트 캐스팅을 지원한다.
 class Num(val value: Int) : Expr
 class Sum(val left: Expr, val right: Expr) : Expr
 
-// 스마트 캐스팅을 지원한다.
 fun eval(e: Expr): Int =
     when (e) {
         is Num -> e.value
@@ -12,18 +12,14 @@ fun eval(e: Expr): Int =
         else -> throw IllegalArgumentException()
     }
 
-/**
- *  클래스의 프로퍼티를 스마트 캐스팅하고 싶다면 val 이면서 커스텀 접근자가 정의되어 있지 않아야 한다.
- *  - var거나 커스텀 접근자가 있으면 언제나 같은 타입을 반환해준다는 것을 확신할 수 없기 때문에..
- */
 fun main() {
     val sum = Sum(Num(3), Num(2))
-    // 스마트 캐스팅 가능
+    // is 로 타입을 한 번 검사하면, 컴파일러가 '해당 변수에 대한 타입이 보장된다'고 판단하고 자동으로 타입을 변환해준다.
     if (sum.left is Num) {
-        println(sum.left.value)
+        println(sum.left.value) // 결과: 3
     }
     if (sum.right is Num) {
-        println(sum.right.value)
+        println(sum.right.value) // 결과: 2
     }
 
     println(eval(Sum(Num(3), Num(2)))) // 결과: 5
