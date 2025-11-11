6324058981c321449e49621f52a9b1c45033c4b2
devfancy <fancy.junyongmoon@gmail.com>
2025-11-11 21:10:19 +0900

    feat: " Kotlin in Action 2/E - 3장 "
    - 문자열과 정규식 관련 코드 추가

--- kotlin-in-action-practice/src/main/kotlin/dev/be/kolininactionpractice/example/Ch03/Chapter03Main.kt
+++ kotlin-in-action-practice/src/main/kotlin/dev/be/kolininactionpractice/example/Ch03/Chapter03Main.kt
@@ -18,6 +18,19 @@ fun main() {
     // NOTE: 3.3.4절 - 확장 함수는 클래스의 멤버가 아니기 때문에 오버라이드가 불가능하다.
     val view: View = Button() // 변수의 '타입'은 View, '실제 객체'는 Button -> 컴파일에 변수의 타입이 호출된다.
     view.showOff() // I'm a view!
+
+    // NOTE: 3.5
+    val ip = "192.168.0.1"
+
+    // 단순 문자열로 분리
+    val partsString = ip.split(".")
+    println("단순 문자열 분리: $partsString") // [192, 168, 0, 1]
+
+    // 모든 문자로 쪼갤 경우 Regex 로 만든다.
+    val partsRegex = ip.split(Regex("\\."))
+    println("정규식으로 분리: $partsString") // [192, 168, 0, 1]
+
+
 }
 
 fun String.lastChar() : Char = this.get(this.length - 1)
